275dcefa207ef9d940fc7798141d955b611dfcc0
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-21 19:57:41 +0000

    css added and api is working great

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ get("/") do
 end
 
 get("/quote") do
+  api_url = "https://zenquotes.io/api/random" 
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  data = parsed_data.at(0)
   
+  @quote = data.fetch("q")
+  @author = data.fetch("a")
   erb(:quote)
 end
